826e9a6613d984819695ff1f2cdd49f4d9272df4
Kostya Stefanovitch <kostia.stefanovitch@rolique.io>
2019-06-22 14:43:14 +0300

    toString handle array values.

--- helpers/toString.js
+++ helpers/toString.js
@@ -1,13 +1,16 @@
-module.exports = (value) => {
+const toString = (value) => {
   if (value == null) { return ''; }
 
   if (typeof value === 'string') {
     return value;
   }
-
+  if (Array.isArray(value)) {
+    return `${value.map(toString)}`;
+  }
   if (typeof value === 'object') {
     return JSON.stringify(value);
   }
-
   return `${value}`;
 };
+
+module.exports = toString;
